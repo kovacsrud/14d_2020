2bccf18f910032557c2b8ead7e49a1c95abdbc64
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-02 11:13:40 +0200

    Egész és lebegőpontos változók

--- Ismetles/Ismetles/Program.cs
+++ Ismetles/Ismetles/Program.cs
@@ -11,6 +11,25 @@ namespace Ismetles
         static void Main(string[] args)
         {
             Console.WriteLine("Ismétlés");
+            //Változók 
+            //A változó a memória egy névvel ellátott része
+
+            //egész típusok
+            int a = -120;
+            uint b = 120;
+            long c = 1000000;
+            byte d = 122;
+            short e = 34;
+
+            //lebegőpontos számok
+            float fl = 10.56f;
+            double db = 10.567788;
+            decimal dec = 10.567788848m;
+
+            
+            
+
+
 
             Console.ReadKey();
 
