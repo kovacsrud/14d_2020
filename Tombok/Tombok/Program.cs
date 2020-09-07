4151e70a9b70791ecca2fe76ef8b5aebebc3c380
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-07 12:50:21 +0200

    Érték és referencia típus közötti különbség

--- Tombok/Tombok/Program.cs
+++ Tombok/Tombok/Program.cs
@@ -10,6 +10,15 @@ namespace Tombok
     {
         static void Main(string[] args)
         {
+            int[] szamok = { 1, 2, 3, 4, 5 };
+            var szamok2 = szamok;
+
+            szamok2[0] = 10;
+
+            //szamok[0] ??????
+
+            Console.WriteLine($"Szamok:{szamok[0]},Szamok2:{szamok2[0]}");
+
 
             Console.ReadLine();
         }
