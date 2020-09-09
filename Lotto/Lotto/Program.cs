f681623cbf291e7cfa8d285eb5fe397e63c358c5
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-09 12:43:43 +0200

    Lotto Start

---
+++ Lotto/Lotto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Készítsen lottójátékot, amelyben adott mennyiségű számból
+            //húzunk ki valamennyi nyerőszámot pl. 6 45-ből, 5 90-ből stb.
+            //A felhasználónak legyen lehetősége ezeket beállítani, de azt 
+            //ne engedjük hogy a tippek száma meghaladja a
+            //nyerőszámok darabszámát
+            // A program kérje be a játékos tippjeit, kétszer ugyanazt a számot
+            //ne fogadja el!
+            //Sorsoljon ki adott számú nyerőszámot
+            //Határozza meg és írja ki a találatok számát
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
