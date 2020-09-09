75550ab9e8e0423b7e160626f46967f0c698b7f5
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-09 08:27:35 +0200

    Listázás

--- KetDimenziosTomb/KetDimenziosTomb/Program.cs
+++ KetDimenziosTomb/KetDimenziosTomb/Program.cs
@@ -22,7 +22,20 @@ namespace KetDimenziosTomb
                 }
             }
 
-
+            //negatívok pirossal, pozitívok zölddel
+            for (int i = 0; i < szamok.GetLength(0); i++)
+            {
+                for (int j = 0; j < szamok.GetLength(1); j++)
+                {
+                    if (szamok[i,j]>0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.Write(szamok[i, j]+" ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
 
 
 
