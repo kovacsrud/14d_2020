583086969acaf107ff8cd25b82206c4ae829d6e1
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-16 09:55:09 +0200

    OOP alapok

---
+++ Allatok/Allatok/Allat.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Allatok
+{
+    public class Allat
+    {
+        //Adattagok
+        private string nev;
+        private int suly;
+
+        public Allat()
+        {
+            this.nev = "A";
+            this.suly = 1;
+        }
+
+
+        public Allat(string nev,int suly)
+        {
+            this.nev = nev;
+            this.suly = suly;
+        }
+
+        //Metódusok
+        public void Mozog()
+        {
+            Console.WriteLine("Az állat mozog");
+        }
+
+        public void Eszik()
+        {
+            Console.WriteLine("Az állat eszik");
+        }
+
+        //public void SetNev(string nev)
+        //{
+        //    this.nev = nev;
+        //}
+
+        public string GetNev()
+        {
+            return nev;
+        }
+    }
+}
---
+++ Allatok/Allatok/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Allatok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Allat allat = new Allat("Ubul",26);
+            Allat masikAllat = new Allat();
+
+
+            //allat.SetNev("Ubul");
+
+            Console.WriteLine(allat.GetNev());
+            allat.Eszik();
+            allat.Mozog();
+
+
+
+            //allat.SetNev("Zénó");
+
+            Console.WriteLine(masikAllat.GetNev());
+
+
+            Console.ReadKey();
+        }
+    }
+}
