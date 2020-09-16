271947ecb2b9e138f456519ba2bce8deffa30b32
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-16 11:08:18 +0200

    Property-k

---
+++ Allatok/Emberek/Ember.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emberek
+{
+    public class Ember
+    {
+        
+
+        public string Nev { get; set; }
+        public int SzuletesiEv { get; set; }
+        public int Magassag { get; set; }
+        public int Suly { get; set; }
+        public int Kor { get; set; }
+
+
+        public Ember()
+        {
+
+        }
+
+        public Ember(string nev, int szuletesiEv, int magassag, int suly, int kor)
+        {
+            Nev = nev;
+            SzuletesiEv = szuletesiEv;
+            Magassag = magassag;
+            Suly = suly;
+            Kor = kor;
+        }
+
+
+
+    }
+}
---
+++ Allatok/Emberek/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emberek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember ember = new Ember();
+
+            ember.Kor = 26;
+            ember.Nev = "Nagy Zénó";
+
+            Ember masikEmber = new Ember("Elek", 1999, 180, 76, 21);
+
+            Console.WriteLine(masikEmber.Nev);
+            Console.WriteLine(masikEmber.SzuletesiEv);
+
+            Console.ReadKey();
+        }
+    }
+}
