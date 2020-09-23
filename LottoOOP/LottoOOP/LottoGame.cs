bebfc9d7549fa690dc28b6868c2aae96d1092635
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-23 12:47:45 +0200

    Lottó OOP verzió

---
+++ LottoOOP/LottoOOP/LottoGame.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoOOP
+{
+    public class LottoGame
+    {
+        private int hanySzam;
+        private int osszSzam;
+        int[] tippek;
+        int[] nyeroSzamok;
+        int talalat;
+        Random rand = new Random();
+
+        public LottoGame(int hanyszam,int osszszam)
+        {
+            hanySzam = hanyszam;
+            osszSzam = osszszam;
+            tippek = new int[hanySzam];
+            nyeroSzamok = new int[hanySzam];
+            talalat = 0;
+
+        }
+
+        public void Tippeles()
+        {
+            for (int i = 0; i < tippek.Length; i++)
+            {
+                Console.Write($"{i+1}.tipp:");
+                var temp = Convert.ToInt32(Console.ReadLine());
+                while (temp>osszSzam || temp<1 || tippek.Contains(temp))
+                {
+                    Console.Write($"Rossz!{i + 1}.tipp:");
+                    temp = Convert.ToInt32(Console.ReadLine());
+                }
+                tippek[i] = temp;
+                Debug.Write(tippek[i] + " ");
+            }
+            
+        }
+
+        public void Sorsolas()
+        {
+            for (int i = 0; i < nyeroSzamok.Length; i++)
+            {
+                var temp = rand.Next(1, osszSzam + 1);
+                while (nyeroSzamok.Contains(temp))
+                {
+                    temp = rand.Next(1, osszSzam + 1);
+                }
+                nyeroSzamok[i] = temp;
+                Debug.Write(nyeroSzamok[i] + " ");
+            }
+
+
+        }
+
+        public int Talalatok()
+        {
+            for (int i = 0; i < tippek.Length; i++)
+            {
+                if (nyeroSzamok.Contains(tippek[i]))
+                {
+                    talalat++;
+                }
+            }
+            return talalat;
+        }
+
+        private void Lista(int[] tomb)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write(tomb[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Tipplista()
+        {
+            Lista(tippek);
+        }
+
+        public void NyeroSzamLista()
+        {
+            Lista(nyeroSzamok);
+        }
+
+
+
+
+    }
+}
---
+++ LottoOOP/LottoOOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A cél, hogy a játék összes tevékenysége egy osztályban
+            //legyen, itt a főprogramban csak minimális kódot akarunk
+            //a játékkal kapcsolatban szerepeltetni
+            LottoGame jatek = new LottoGame(5, 90);
+            jatek.Tippeles();
+            jatek.Sorsolas();
+            jatek.Tipplista();
+            jatek.NyeroSzamLista();
+            Console.WriteLine($"Találat:{jatek.Talalatok()}");
+
+            Console.ReadKey();
+        }
+    }
+}
