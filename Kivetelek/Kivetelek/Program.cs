e7f4692c2458fdd2321c855080d6a9dcccce414e
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-05 12:07:16 +0200

    Kivételek

---
+++ Kivetelek/Kivetelek/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetelek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 0;
+                try
+                {
+                    int c = a / b;
+                    Console.WriteLine(c);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Nullával való osztás");                    
+                }
+
+                Console.WriteLine("A program folytatódik");
+
+                throw new ArgumentException();
+
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine("Ez bizony null!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                
+            }
+            finally
+            {
+                Console.WriteLine("Ami itt van mindig lefut");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
