ea26bea83e32bd4c649f4746e5a3b67c2fb3c997
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-05 13:30:03 +0200

    Interfesz

---
+++ Interfeszek/Interfeszek/Kor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfeszek
+{
+    public class Kor : ISikidom
+    {
+        
+
+        public double Sugar { get; set; }
+
+        public Kor(double sugar)
+        {
+            Sugar = sugar;
+        }
+        
+
+        public double Kerulet()
+        {
+            return 2 * Sugar * Math.PI;
+        }
+
+        public double Terulet()
+        {
+            return Sugar * Sugar * Math.PI;
+        }
+    }
+}
---
+++ Interfeszek/Interfeszek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfeszek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tudunk-e különböző osztályú objektumot
+            //azonos listában tárolni?
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
