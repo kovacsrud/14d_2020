0052125ab18e616b3cfef64b502a33a01fb5cc8d
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-05 13:15:35 +0200

    Using minta

---
+++ UsingMinta/UsingMinta/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace UsingMinta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (!Console.KeyAvailable)
+            {
+
+                using (var bitmap = new Bitmap(1280, 1024))
+                {
+
+                    Console.WriteLine(GC.GetTotalMemory(false));
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
