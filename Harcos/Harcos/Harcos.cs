c42df1b7a8119519491ddd0dd749c608fb1eb836
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-14 11:45:54 +0200

    Harcos feladat

---
+++ Harcos/Harcos/Harcos.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harcos
+{
+    public class Harcos
+    {
+        private string nev;
+        private int eletero;
+        private int harciero;
+
+        public string Nev
+        {
+            get
+            {
+                return nev;
+            }
+
+           
+        }
+
+        public int Eletero
+        {
+            get
+            {
+                return eletero;
+            }
+
+            set
+            {
+                eletero = value;
+            }
+        }
+
+        public int Harciero
+        {
+            get
+            {
+                return harciero;
+            }
+                     
+        }
+
+        public Harcos(string nev, int eletero, int harciero)
+        {
+            this.nev = nev;
+            this.eletero = eletero;
+            this.harciero = harciero;
+        }
+
+        public override string ToString()
+        {
+            return $"Név:{this.nev},Életerő:{this.eletero},Harci erő:{this.harciero}";
+        }
+
+        public bool Harcol(Harcos ellenfel)
+        {
+            ellenfel.Eletero = ellenfel.Eletero - this.harciero;
+            this.eletero = this.eletero - ellenfel.Harciero;
+            if (this.eletero <= 0 || ellenfel.Eletero <= 0)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Harcos/Harcos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harcos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Harcos h1 = new Harcos("Yoda", 100, 50);
+            Harcos h2 = new Harcos("Dookoo",100,30);
+            while (!h1.Harcol(h2))
+            {
+                h1.Harcol(h2);
+                Console.WriteLine(h1.ToString());
+                Console.WriteLine(h2.ToString());
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
