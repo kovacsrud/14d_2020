5431eb86e928fbef50cff35643a1c03d8fbd3988
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-19 11:49:15 +0200

    Update Jarmu.cs

--- AutoOOP/AutoOOP/Jarmu.cs
+++ AutoOOP/AutoOOP/Jarmu.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace AutoOOP
 {
-    class Jarmu
+    public abstract class Jarmu
     {
+        public int sebesseg;
+        private string rendszam;
+
+        public Jarmu(int sebesseg, string rendszam)
+        {
+            this.sebesseg = sebesseg;
+            this.rendszam = rendszam;
+        }
+
+        public abstract bool GyorsHajtottE(int sebessegkorlat);
+
+        public override string ToString()
+        {
+            return $"{this.rendszam}-{this.sebesseg} km/h";
+        }
     }
 }
