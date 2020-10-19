02bfcf025216c839df3578730e386e104c942e38
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-19 13:43:10 +0200

    Orszagut osztály

--- AutoOOP/AutoOOP/Orszagut.cs
+++ AutoOOP/AutoOOP/Orszagut.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,38 @@ namespace AutoOOP
         public Orszagut(string fajl)
         {
             jarmuvek = new List<Jarmu>();
+            try
+            {
+                var sorok = File.ReadAllLines(fajl, Encoding.Default);
+                for (int i = 0; i < sorok.Length; i++)
+                {
+                    var e = sorok[i].Split(';');
+                    if (e[0]=="robogo")
+                    {
+                        Robogo robogo = new Robogo(
+                            Convert.ToInt32(e[2]),
+                            e[1],
+                            Convert.ToInt32(e[3])
+                            );
+                        jarmuvek.Add(robogo);
+                    }
+                    else
+                    {
+                        AudiS8 audi = new AudiS8(
+                            Convert.ToInt32(e[2]),
+                            e[1],
+                            Convert.ToBoolean(e[3])
+                            );
+                        jarmuvek.Add(audi);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+            
 
         }
         
