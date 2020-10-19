64ed0da01801d5c46fe6e0f788db6ac770076361
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-19 12:41:31 +0200

    Create AudiS8.cs

---
+++ AutoOOP/AutoOOP/AudiS8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoOOP
+{
+    public class AudiS8:Jarmu
+    {
+        private bool lezerblokkolo;
+
+        public AudiS8(int sebesseg, string rendszam,bool lezerblokkolo) : base(sebesseg, rendszam)
+        {
+            this.lezerblokkolo = lezerblokkolo;
+        }
+
+        public override bool GyorsHajtottE(int sebessegkorlat)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
