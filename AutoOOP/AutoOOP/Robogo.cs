d3ef0846a7fe97fd59b713e40581007c2665010c
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-19 11:56:24 +0200

    Create Robogo.cs

---
+++ AutoOOP/AutoOOP/Robogo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoOOP
+{
+    public class Robogo : Jarmu, IKisGepjarmu
+    {
+        private int maxSebesseg;
+
+        public Robogo(int sebesseg, string rendszam,int maxsebesseg) : base(sebesseg, rendszam)
+        {
+            maxSebesseg = maxsebesseg;
+        }
+
+        public override bool GyorsHajtottE(int sebessegkorlat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HaladhatItt(int sebesseg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
