a77be2c9df7c6f0d6df0f7ee898e3c8590735d93
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-21 11:49:58 +0200

    Pekseg start

---
+++ Pekseg/Pekseg/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pekseg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Arak.CSESZEKAVE);
+            Console.ReadKey();
+        }
+    }
+}
