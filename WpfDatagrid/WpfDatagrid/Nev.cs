4fee82195556ca8d0a746b60ab063ae8582a47d8
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-11 11:10:31 +0100

    Nev

---
+++ WpfDatagrid/WpfDatagrid/Nev.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDatagrid
+{
+    public class Nev
+    {
+        public string Vezeteknek { get; set; }
+        public string Keresztnev { get; set; }
+        public int SzuletesiEv { get; set; }
+        public string SzuletesiHely { get; set; }
+    }
+}
