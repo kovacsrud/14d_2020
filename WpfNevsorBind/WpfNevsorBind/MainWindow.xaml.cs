138d2313d92708bb80c5e8e52433d17b8d243ce8
szajberpiraty <szajberpiraty@gmail.com>
2020-11-16 14:05:27 +0100

    Névsor adatkötés példák

---
+++ WpfNevsorBind/WpfNevsorBind/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfNevsorBind
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Nevsor nevsorAdatok;
+        public MainWindow()
+        {
+            InitializeComponent();
+            nevsorAdatok = new Nevsor("tesztadat_20k.txt");
+            //adatok.ItemsSource = nevsorAdatok.NevSor;
+            adatok.DataContext = nevsorAdatok;
+        }
+
+        private void buttonKeres_Click(object sender, RoutedEventArgs e)
+        {
+            veznevDatagrid.DataContext = nevsorAdatok.VezetekNevKereses(textboxKereses.Text);
+        }
+    }
+}
---
+++ WpfNevsorBind/WpfNevsorBind/Nev.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfNevsorBind
+{
+    public class Nev
+    {
+        public string Vezeteknev { get; set; }
+        public string Keresztnev { get; set; }
+        public int SzuletesiEv { get; set; }
+        public string SzuletesiHely { get; set; }
+
+    }
+}
