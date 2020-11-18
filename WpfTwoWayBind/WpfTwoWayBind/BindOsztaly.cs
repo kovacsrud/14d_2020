d9ab1102dfeb949074b97fa1e3e8247531db0653
szajberpiraty <szajberpiraty@gmail.com>
2020-11-18 13:21:13 +0100

    Binding

---
+++ WpfTwoWayBind/WpfTwoWayBind/BindOsztaly.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTwoWayBind
+{
+    public class BindOsztaly:INotifyPropertyChanged
+    {
+        private int bindadat;
+        public int BindAdat {
+            get { return bindadat; }
+            set
+            {
+                if (value!=bindadat && value>=0 && value<=100)
+                {
+                    bindadat = value;
+                    Prop_Changed("BindAdat");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Prop_Changed(string propnev)
+        {
+            if (PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propnev));
+            }
+        }
+    }
+}
