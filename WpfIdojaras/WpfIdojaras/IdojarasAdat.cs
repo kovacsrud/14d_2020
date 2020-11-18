fd2ae1abb5dfb9ead96c6f3422a55533884c787c
szajberpiraty <szajberpiraty@gmail.com>
2020-11-18 08:30:37 +0100

    WpfIdojaras start

---
+++ WpfIdojaras/WpfIdojaras/IdojarasAdat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfIdojaras
+{
+    public class IdojarasAdat
+    {
+        public int Ev { get; set; }
+        public int Honap { get; set; }
+        public int Nap { get; set; }
+        public int Ora { get; set; }
+        public double Homerseklet { get; set; }
+        public double Szelsebesseg { get; set; }
+        public double Paratartalom { get; set; }
+    }
+}
---
+++ WpfIdojaras/WpfIdojaras/IdojarasAdatok.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfIdojaras
+{
+    public class IdojarasAdatok
+    {
+        private List<IdojarasAdat> idojarasadatok;
+    }
+}
