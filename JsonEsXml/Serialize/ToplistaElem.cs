156e63e7efa9139ab07edc52eabb2cb0370dec68
szajberpiraty <szajberpiraty@gmail.com>
2020-11-30 12:41:17 +0100

    Toplista start

---
+++ JsonEsXml/Serialize/ToplistaElem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialize
+{
+    public class ToplistaElem
+    {
+        public string Nev { get; set; }
+        public int Pontszam { get; set; }
+    }
+}
