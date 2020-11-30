76d4d1c4f1cd4f83a44d6df258a7c693f0369881
szajberpiraty <szajberpiraty@gmail.com>
2020-11-30 13:20:57 +0100

    Szerializáció

--- JsonEsXml/Serialize/Program.cs
+++ JsonEsXml/Serialize/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Serialize
 {
@@ -10,6 +12,30 @@ namespace Serialize
     {
         static void Main(string[] args)
         {
+            List<ToplistaElem> toplista = new List<ToplistaElem>();
+            toplista.Add(new ToplistaElem {Nev="Destroyer",Pontszam=2677 });
+            toplista.Add(new ToplistaElem { Nev = "Fatality", Pontszam = 2388 });
+            toplista.Add(new ToplistaElem { Nev = "NoPingJustLag", Pontszam = 2188 });
+            toplista.Add(new ToplistaElem { Nev = "Noob", Pontszam = 1988 });
+
+            //Szerializáció XML fájlokkal
+            var xmlFajl = "toplista.xml";
+            var serializer = new XmlSerializer(typeof(List<ToplistaElem>));
+            using (var fs = new FileStream(xmlFajl, FileMode.Create))
+            {
+                serializer.Serialize(fs, toplista);
+            }
+
+            List<ToplistaElem> betolt;
+            using (var fs=new FileStream(xmlFajl,FileMode.Open))
+            {
+                betolt = (List<ToplistaElem>)serializer.Deserialize(fs);
+            }
+
+            foreach (var i in betolt)
+            {
+                Console.WriteLine($"{i.Nev},{i.Pontszam}");
+            }
 
 
             Console.ReadKey();
