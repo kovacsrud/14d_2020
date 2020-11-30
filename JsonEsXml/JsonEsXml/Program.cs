da4186bb29061d49e30ca01c3aacdb92486e5b28
szajberpiraty <szajberpiraty@gmail.com>
2020-11-30 11:55:34 +0100

    Json olvasás

--- JsonEsXml/JsonEsXml/Program.cs
+++ JsonEsXml/JsonEsXml/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace JsonEsXml
 {
@@ -11,7 +13,23 @@ namespace JsonEsXml
     {
         static void Main(string[] args)
         {
+            string jsonFajl = "";
+            try
+            {
+                jsonFajl = File.ReadAllText("colors.json",Encoding.Default);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
 
+            //Console.WriteLine(jsonFajl);
+            JsonTextReader reader = new JsonTextReader(new StringReader(jsonFajl));
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader.TokenType},{reader.ValueType},{reader.Value}");
+            }
 
 
             Console.ReadKey();
