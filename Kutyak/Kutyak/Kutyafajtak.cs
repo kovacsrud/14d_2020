02b583c65a23094f24da154026621e9a509c8068
szajberpiraty <szajberpiraty@gmail.com>
2020-12-11 11:12:32 +0100

    Kutyafajtak

--- Kutyak/Kutyak/Kutyafajtak.cs
+++ Kutyak/Kutyak/Kutyafajtak.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,42 @@ namespace Kutyak
 {
     public class Kutyafajtak
     {
+        public class Kutyafajta
+        {
+            public int Id { get; set; }
+
+            public string Nev { get; set; }
+            public string Eredetinev { get; set; }
+
+        }
+        private List<Kutyafajta> kutyafajtaadatok;
+        public List<Kutyafajta> Kutyafajtaadatok { get; }
+
+        public Kutyafajtak(string fajl)
+        {
+            kutyafajtaadatok = new List<Kutyafajta>();
+            try
+            {
+                var sorok = File.ReadAllLines(fajl, Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    var e = sorok[i].Split(';');
+                    kutyafajtaadatok.Add(
+                        new Kutyafajta
+                        {
+                            Id = Convert.ToInt32(e[0]),
+                            Nev = e[1],
+                            Eredetinev=e[2]
+
+                        }
+                        );
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
