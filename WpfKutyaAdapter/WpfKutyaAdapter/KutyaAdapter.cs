8038777eaad4907c36beb4ad5bdb6f691c9ebf96
szajberpiraty <szajberpiraty@gmail.com>
2021-01-06 10:43:24 +0100

    Create KutyaAdapter.cs

---
+++ WpfKutyaAdapter/WpfKutyaAdapter/KutyaAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfKutyaAdapter
+{
+    public class KutyaAdapter
+    {
+        string connString;
+
+
+        public KutyaAdapter(string connstring)
+        {
+            connString = connstring;
+        }
+    }
+}
