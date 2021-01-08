6dcbeada21ce26f68278af67be278ea63255c1a6
szajberpiraty <szajberpiraty@gmail.com>
2021-01-08 10:37:15 +0100

    Update FajtaAdapter.cs

--- WpfKutyaAdapter/WpfKutyaAdapter/FajtaAdapter.cs
+++ WpfKutyaAdapter/WpfKutyaAdapter/FajtaAdapter.cs
@@ -1,6 +1,24 @@
-﻿namespace WpfKutyaAdapter
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SQLite;
+using System.Threading.Tasks;
+
+namespace WpfKutyaAdapter
 {
     public class FajtaAdapter
     {
+        string connString;
+        DataTable fajtaadatok;
+        public DataView Fajtaadatok { get { return fajtaadatok.DefaultView; } }
+        SQLiteDataAdapter adapter;
+        SQLiteConnection conn;
+
+        public FajtaAdapter()
+        {
+
+        }
     }
 }
