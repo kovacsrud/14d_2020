4c9b4820c4269ed48df1d0a49a007ab83ef55051
szajberpiraty <szajberpiraty@gmail.com>
2021-01-13 08:15:51 +0100

    Update KezelesAdapter.cs

--- WpfKutyaAdapter/WpfKutyaAdapter/KezelesAdapter.cs
+++ WpfKutyaAdapter/WpfKutyaAdapter/KezelesAdapter.cs
@@ -33,7 +33,30 @@ namespace WpfKutyaAdapter
             adapter.InsertCommand.Parameters.Add("@fajtaid",DbType.Int32,0,"fajtaid");
             adapter.InsertCommand.Parameters.Add("@nevid",DbType.Int32,0,"nevid");
             adapter.InsertCommand.Parameters.Add("@eletkor", DbType.Int32, 0, "eletkor");
-            adapter.InsertCommand.Parameters.Add("@utolsoell", DbType.Int32, 0, "utolsoell");
+            adapter.InsertCommand.Parameters.Add("@utolsoell", DbType.String, 0, "utolsoell");
+
+            adapter.UpdateCommand = new SQLiteCommand(conn);
+            adapter.UpdateCommand.CommandText = "update kutyak set " +
+                "fajtaid=@fajtaid,nevid=@nevid,eletkor=@eletkor,utolsoell=@utolsoell " +
+                "where id=@id";
+            adapter.UpdateCommand.Parameters.Add("@fajtaid", DbType.Int32, 0, "fajtaid");
+            adapter.UpdateCommand.Parameters.Add("@nevid", DbType.Int32, 0, "nevid");
+            adapter.UpdateCommand.Parameters.Add("@eletkor", DbType.Int32, 0, "eletkor");
+            adapter.UpdateCommand.Parameters.Add("@utolsoell", DbType.String, 0, "utolsoell");
+            adapter.UpdateCommand.Parameters.Add("@id",DbType.Int32,0,"id").SourceVersion=DataRowVersion.Original;
+
+            adapter.DeleteCommand = new SQLiteCommand(conn);
+            adapter.DeleteCommand.CommandText = "delete from kutyak where id=@id";
+            adapter.DeleteCommand.Parameters.Add("@id", DbType.Int32, 0, "id").SourceVersion = DataRowVersion.Original;
+
+            adapter.Fill(kezelesadatok);
+        }
+
+        public void UpdateKutyak()
+        {
+            adapter.Update(kezelesadatok);
+            kezelesadatok.Clear();
+            adapter.Fill(kezelesadatok);
         }
 
     }
