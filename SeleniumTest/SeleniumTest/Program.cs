9da0bb96f3f744a0fa8ea54916fd291c16e423d5
szajberpiraty <szajberpiraty@gmail.com>
2021-03-17 11:57:07 +0100

    Update Program.cs

--- SeleniumTest/SeleniumTest/Program.cs
+++ SeleniumTest/SeleniumTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -21,6 +22,20 @@ namespace SeleniumTest
             driver.FindElement(By.Id("password")).SendKeys("a1234567"+Keys.Enter);
             driver.FindElement(By.LinkText("Programozás")).Click();
             driver.FindElement(By.LinkText("Szoftverfejlesztő (14d13b)")).Click();
+
+            //IList<IWebElement> szekcio = driver.FindElements(By.XPath("/html/body/div[2]/div/div[4]/div/div/div[1]/div/div/div/ul/li[2]/div[3]"));
+            //IList<IWebElement> szekcio = driver.FindElements(By.XPath("/html/body/div[2]/div/div[4]/div/div/div[1]/div/div/div/ul/li[2]"));
+            driver.FindElement(By.XPath("//span[contains(.,'Akna pozíció generáló osztály feladat feltöltése (.zip-ben)')]")).Click();
+
+
+            //for (int i = 0; i < szekcio.Count; i++)
+            //{
+            //    Console.WriteLine(szekcio[i].Text);
+            //}
+
+            Thread.Sleep(5000);
+
+            driver.Close();
 
 
 
