027ef89e1e699f385dea11a2abfe92fb88a8d2ff
szajberpiraty <szajberpiraty@gmail.com>
2021-03-22 13:10:12 +0100

    Teszt start

---
+++ DllTest/DllTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KRHash;
+namespace DllTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHash hash = new CreateHash();
+            Console.WriteLine(hash.MakeHash(HashType.SHA1,"valami"));
+            Console.ReadKey();
+        }
+    }
+}
