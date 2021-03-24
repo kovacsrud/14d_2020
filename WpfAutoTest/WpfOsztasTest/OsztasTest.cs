3cce2041e3ed62e63efa2978c7ea9af3ff72071c
szajberpiraty <szajberpiraty@gmail.com>
2021-03-24 11:06:31 +0100

    Update OsztasTest.cs

--- WpfAutoTest/WpfOsztasTest/OsztasTest.cs
+++ WpfAutoTest/WpfOsztasTest/OsztasTest.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using OpenQA.Selenium;
-
+using System;
 
 namespace WpfOsztasTest
 {
@@ -10,21 +10,42 @@ namespace WpfOsztasTest
     {
         protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
 
-        private const string WpfProgramId = @"G:\c#_uj_projektek\14d_2000\WpfAutoTest\WpfAutoTest\WpfAutoTest.exe";
+        private const string WpfProgramId = @"G:\c#_uj_projektek\14d_2000\WpfAutoTest\WpfAutoTest\bin\Debug\WpfAutoTest.exe";
 
-        private const string WpfProgramPath = @"G:\c#_uj_projektek\14d_2000\WpfAutoTest\WpfAutoTest\";
+        private const string WpfProgramPath = @"G:\c#_uj_projektek\14d_2000\WpfAutoTest\WpfAutoTest\bin\Debug\";
 
         protected static WindowsDriver<WindowsElement> driver;
 
-        [SetUp]
-        public void Setup()
+        [OneTimeSetUp]
+        public static void Setup()
         {
+            if (driver==null)
+            {
+                var appiumoptions = new AppiumOptions();
+                appiumoptions.AddAdditionalCapability("app",WpfProgramId);
+                appiumoptions.AddAdditionalCapability("deviceName","WindowsPC");
+                driver = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appiumoptions);
+
+            }
+
         }
 
         [Test]
-        public void Test1()
+        public void OsztasTest()
         {
-            Assert.Pass();
+            var aErtek = driver.FindElementByAccessibilityId("textboxA");
+            var bErtek = driver.FindElementByAccessibilityId("textboxB");
+            var eredmeny = driver.FindElementByAccessibilityId("textblockEredmeny");
+            aErtek.Clear();
+            bErtek.Clear();
+            eredmeny.Clear();
+            aErtek.SendKeys("2");
+            bErtek.SendKeys("4");
+            driver.FindElementByAccessibilityId("buttonSzamol").Click();
+            eredmeny = driver.FindElementByAccessibilityId("textblockEredmeny");
+            Assert.AreEqual(0.5, Convert.ToDouble(eredmeny));
+
+
         }
     }
 }
