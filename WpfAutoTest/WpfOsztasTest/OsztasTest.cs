70486ce221c8f8b5d0fb12c0cff55f6c1823f03f
szajberpiraty <szajberpiraty@gmail.com>
2021-03-24 12:53:46 +0100

    Update OsztasTest.cs

--- WpfAutoTest/WpfOsztasTest/OsztasTest.cs
+++ WpfAutoTest/WpfOsztasTest/OsztasTest.cs
@@ -3,6 +3,10 @@ using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using OpenQA.Selenium;
 using System;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using NUnit.Framework.Interfaces;
 
 namespace WpfOsztasTest
 {
@@ -15,6 +19,10 @@ namespace WpfOsztasTest
         private const string WpfProgramPath = @"G:\c#_uj_projektek\14d_2000\WpfAutoTest\WpfAutoTest\bin\Debug\";
 
         protected static WindowsDriver<WindowsElement> driver;
+
+        protected static ExtentReports extReport;
+
+        protected static ExtentTest extTest;
 
         [OneTimeSetUp]
         public static void Setup()
@@ -30,6 +38,19 @@ namespace WpfOsztasTest
 
         }
 
+        [OneTimeSetUp]
+        public static void ReportSetup()
+        {
+            extReport = new ExtentReports();
+            var htmlReporter = new ExtentHtmlReporter(@"G:\14d_extrep\");
+            extReport.AddSystemInfo("Osztás test", "Auto teszt");
+            extReport.AddSystemInfo("Tesztelõ:", "KR");
+            extReport.AttachReporter(htmlReporter);
+            htmlReporter.Config.DocumentTitle = "HTML teszt riport";
+            htmlReporter.Config.ReportName = "Osztás autoteszt";
+            htmlReporter.Config.Theme = Theme.Standard;
+        }
+
         [Test]
         [TestCase(1,2,0.5)]
         [TestCase(2,1,2)]
@@ -38,19 +59,48 @@ namespace WpfOsztasTest
         [TestCase(56,2,28)]
         public void OsztasTest(double a,double b,double elvart)
         {
+            extTest = extReport.CreateTest("Osztás teszt");
             var aErtek = driver.FindElementByAccessibilityId("textboxA");
             var bErtek = driver.FindElementByAccessibilityId("textboxB");
             var eredmeny = driver.FindElementByAccessibilityId("textblockEredmeny");
             aErtek.Clear();
             bErtek.Clear();
             eredmeny.Clear();
-            aErtek.SendKeys(a.ToString());
-            bErtek.SendKeys(b.ToString());
+            aErtek.SendKeys(Convert.ToString(a));
+            bErtek.SendKeys(Convert.ToString(b));
             driver.FindElementByAccessibilityId("buttonSzamol").Click();
             eredmeny = driver.FindElementByAccessibilityId("textblockEredmeny");
             Assert.AreEqual(elvart, Convert.ToDouble(eredmeny.Text));
+            extTest.Log(Status.Pass, "Osztás teszt rendben");
+
+        }
+
+        [TearDown]
+        public static void CloseReport()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stacktrace = TestContext.CurrentContext.Result.StackTrace;
+            var errormsg = TestContext.CurrentContext.Result.Message;
+
+            if (status==TestStatus.Failed)
+            {
+                ITakesScreenshot shot = (ITakesScreenshot)driver;
+                var a = TestContext.CurrentContext.Test.Arguments.GetValue(0);
+                var b = TestContext.CurrentContext.Test.Arguments.GetValue(1);
+                var elvart = TestContext.CurrentContext.Test.Arguments.GetValue(2);
+                string fajlnev = a + "_" + b + "_" + elvart;
+                Screenshot screenshot = shot.GetScreenshot();
 
 
+
+            }
+        }
+
+        [OneTimeTearDown]
+        public void EndTest()
+        {
+            extReport.Flush();
+            
         }
     }
 }
